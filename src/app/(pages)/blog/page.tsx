5fce6e46564546daabb3bd475c3e0f788ea8e573
Zlaam <mrzalaam@gmail.com>
2024-05-07 01:03:52 +0500

    create pages

---
+++ src/app/(pages)/blog/page.tsx
@@ -0,0 +1,11 @@
+import {} from "react";
+
+function BlogPage() {
+  return (
+    <>
+      <section>BlogPage</section>
+    </>
+  );
+}
+
+export default BlogPage;
---
+++ src/app/(pages)/contact/page.tsx
@@ -0,0 +1,11 @@
+import {} from "react";
+
+function ContactPage() {
+  return (
+    <>
+      <section>ContactPage</section>
+    </>
+  );
+}
+
+export default ContactPage;
