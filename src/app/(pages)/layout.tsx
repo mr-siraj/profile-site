a8830d78b3d33a974db8be824d9cc66548dfc42c
Zlaam <mrzalaam@gmail.com>
2024-05-07 03:06:26 +0500

    update layout.tsx

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,10 +1,13 @@
 import {} from "react";
+import Header from "@/app/components/Header/Header";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main className="flex  flex-col relative min-h-screen bg-red-500 ">
-        <section className="h-[70px] bg-blue-500 sticky top-0">Layout</section>
+        <section className="h-[70px] sticky top-0">
+          <Header />
+        </section>
         <section className="flex-[1] flex-grow">{children}</section>
       </main>
     </>
