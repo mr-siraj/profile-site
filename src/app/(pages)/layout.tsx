ccbc652d0bf9d95266f6dc488f2f1838a510a283
Zlaam <mrzalaam@gmail.com>
2024-05-07 17:50:38 +0500

    update layout.tsx

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -7,7 +7,21 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main className="flex pattern_dark  flex-col relative min-h-screen">
-        <NextTopLoader />
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={1}
+          crawlSpeed={200}
+          height={3}
+          crawl={false}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />{" "}
         <section className="sticky top-0">
           <Header />
         </section>
