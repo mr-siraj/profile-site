78eb5fbc0b0a3578968b3fc5dc8853b2cb58a42b
Zlaam <mrzalaam@gmail.com>
2024-05-07 15:49:11 +0500

    update 6 files and create 1 file

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -3,18 +3,17 @@ import Link from "next/link";
 import {} from "react";
 import NavLinks from "./NavLinks";
 import Image from "next/image";
-import { Menu } from "lucide-react";
+
 function Header() {
   return (
     <>
-      <section className="h-[70px] flex justify-between items-center md:mx-[70px] mx-10">
+      <section className="h-[100px] flex justify-between items-center  lg :mx-[70px] mx-10 ">
         <div className="logo">
           <Link href="/home">
             <Image src="/logo/image.png" alt="zlaam" width={75} height={75} />
           </Link>
         </div>
         <NavLinks />
-        <Menu />
       </section>
     </>
   );
--- src/app/components/Header/NavLinks.tsx
+++ src/app/components/Header/NavLinks.tsx
@@ -1,46 +1,61 @@
 "use client";
-import {} from "react";
+import { Fragment, useState } from "react";
+import { Menu } from "lucide-react";
+import { Divide as Hamburger } from "hamburger-react";
 import { useActivePath } from "@/app/helper/CeckActivePath/CheckActivePath";
 import Link from "next/link";
+import { cn } from "@/app/lib/utils";
 function NavLinks() {
+  const [isOpen, setOpen] = useState(false);
   const isActive = useActivePath();
   const navLinks = [
-    {
-      name: "Home",
-      path: "/home",
-    },
-    {
-      name: "Expertise",
-      path: "/expertise",
-    },
-    {
-      name: "Projects",
-      path: "/projects",
-    },
-    {
-      name: "Contact",
-      path: "/contact",
-    },
-    {
-      name: "Blogs",
-      path: "/blogs",
-    },
+    { id: 1, name: "Home", path: "/home" },
+    { id: 2, name: "Expertise", path: "/expertise" },
+    { id: 3, name: "Projects", path: "/projects" },
+    { id: 4, name: "Contact", path: "/contact" },
+    { id: 5, name: "Blogs", path: "/blogs" },
   ];
+  const handleCloseMenu = () => {
+    setOpen(false);
+  };
   return (
     <>
-      <nav className="flex gap-10 items-center">
+      <nav
+        className={cn(
+          `flex flex-col h-screen justify-center absolute top-0 right-0 pattern w-full items-center gap-20 
+      md:flex-row md:h-0 md:static md:gap-10 md:justify-end overflow-y-auto`,
+          !isOpen
+            ? "translate-x-[-100%] duration-300 transition-all"
+            : "translate-x-0 duration-300 transition-all"
+        )}
+      >
         {navLinks.map((data) => (
-          <Link
-            href={data.path && data.path}
-            key={data.name}
-            className={`link_light relative tracking-wide text-[1.2rem] ${
-              isActive(data.path) ? "text-lg font-medium" : ""
-            }`}
-          >
-            {data.name}
-          </Link>
+          <Fragment key={data.id}>
+            <Link
+              onClick={handleCloseMenu}
+              href={data.path && data.path}
+              className={`link_light relative tracking-wide text-[1.2rem] ${
+                isActive(data.path) ? "text-lg font-medium" : ""
+              }`}
+            >
+              {data.name}
+            </Link>
+          </Fragment>
         ))}
       </nav>
+      <div
+        className="md:hidden absolute top-8 right-10"
+        onClick={() => setOpen(!isOpen)}
+      >
+        <Hamburger
+          distance="sm"
+          toggled={isOpen}
+          toggle={setOpen}
+          size={25}
+          rounded
+          hideOutline={false}
+        />
+      </div>
     </>
   );
 }
