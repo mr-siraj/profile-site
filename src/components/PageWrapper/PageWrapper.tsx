19c7121aeaab76bb7a321fe96c9bc1487eaecf09
Zlaam <mrzalaam@gmail.com>
2024-05-07 20:53:39 +0500

    update PageWrapper.tsx

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -10,7 +10,7 @@ const PageWrapper = ({
   return (
     <div
       className={cn(
-        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        "mx-auto w-full max-w-screen-lg px-2.5 md:px-20",
         className
       )}
     >
