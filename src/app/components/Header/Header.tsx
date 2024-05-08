388c7f05c93865e4db852db5dafcccc3403f6377
Zlaam <mrzalaam@gmail.com>
2024-05-09 02:20:33 +0500

    update next.config.mjs and Header.tsx

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,7 +2,6 @@
 import PageWrapper from "@/components/PageWrapper/PageWrapper";
 import Image from "next/image";
 import Link from "next/link";
-import {} from "react";
 import NavLinks from "./NavLinks";
 import { usePathname } from "next/navigation";
 
@@ -15,7 +14,12 @@ function Header() {
         <PageWrapper className="h-[100px] flex justify-between items-center  lg:px-[70px] md:py-20 pattern_dark text-white">
           <div className="ml-5 mt-2 md:-ml-5 ">
             <Link href="/home" className="select-none">
-              <Image src="/logo/image.png" alt="zlaam" width={75} height={75} />
+              <Image
+                src="./logo/image.png"
+                alt="zlaam"
+                width={75}
+                height={75}
+              />
             </Link>
           </div>
           <NavLinks />
