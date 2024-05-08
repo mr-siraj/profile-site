6a12a7c68acb0547b7ce040e0d4e12c5e19965d3
Zlaam <mrzalaam@gmail.com>
2024-05-08 22:18:44 +0500

    update 1 file and create 2 files

--- src/components/Expertise_Card/Expertise_Card.tsx
+++ src/components/Expertise_Card/Expertise_Card.tsx
@@ -1,6 +1,7 @@
-import {} from "react";
+import { Suspense } from "react";
 import "./Expertise_Card.scss";
 import Image from "next/image";
+import ImageLoader from "../ImgLoader/ImageLoader";
 interface ExpertiseData {
   title: string;
   content: string;
@@ -10,11 +11,21 @@ function Expertise_Card({ data }: { data: ExpertiseData }) {
   return (
     <>
       <div className="card">
-        <Image src={data.image} alt={data.title} height={100} width={100} />
+        <Suspense
+          fallback={
+            <div>
+              <ImageLoader />
+            </div>
+          }
+        >
+          <Image src={data?.image} alt={data?.title} height={100} width={100} />
+        </Suspense>
         <div className="card__content backdrop-blur-sm p-5">
-          <h1 className="card__title font-bold text-[1.25rem]">{data.title}</h1>
+          <h1 className="card__title font-bold text-[1.25rem]">
+            {data?.title}
+          </h1>
           <p className="card__description  text-sm sm:font-semibold">
-            {data.content}
+            {data?.content}
           </p>
         </div>
       </div>
