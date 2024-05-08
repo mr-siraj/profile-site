3fb5c1c49a9520b1b0e7c1de568f92c12b21dfb2
Zlaam <mrzalaam@gmail.com>
2024-05-09 02:06:49 +0500

    update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
 import {} from "react";
 
 function NotFound() {
   return (
     <>
-      <section className="pattern_dark  text-4xl">NotFound</section>
+      <section className="pattern_dark  text-4xl bg-red-500 border border-white gap-10 h-screen flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold">404 | Not Found</h1>
+        <p>
+          <Link href="/home" className="text-3xl font-semibold link_dark">
+            Go Home
+          </Link>
+        </p>
+      </section>
     </>
   );
 }
