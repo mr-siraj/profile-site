ba92ab8c1fe12ece5df270c90eaabb77f933a501
Zlaam <mrzalaam@gmail.com>
2024-05-08 21:30:43 +0500

    add expertise card

--- src/app/(pages)/expertise/page.tsx
+++ src/app/(pages)/expertise/page.tsx
@@ -1,9 +1,11 @@
+import Expertise_Card from "@/components/Expertise_Card/Expertise_Card";
+import expertiseData from "./expertise.json";
 export default function ExpertisePage() {
   return (
     <section className="flex flex-col">
       <div className="my-2 flex-1">
         <h1 className="font-bold md:text-5xl text-3xl">My Expertise!</h1>
-        <h2 className="leading-[2rem] tracking-wider text-xl my-2">
+        <h2 className="leading-[2rem] tracking-wider text-xl my-4">
           I am proficient in languages such as JavaScript and Python, and I
           specialize in creating dynamic and responsive web applications. My
           goal is to continue pushing the boundaries of innovation in the
@@ -12,8 +14,17 @@ export default function ExpertisePage() {
       </div>
       <div className="my-2 flex-1 flex flex-col">
         <h1 className="font-bold md:text-5xl text-3xl">
-          Technologies I use in Daily Life:-
+          Technologies I use in Daily Life :
         </h1>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2  grid-cols-1">
+          {expertiseData?.map((data) => {
+            return (
+              <div key={data.title}>
+                <Expertise_Card data={data} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Expertise_Card/Expertise_Card.tsx
@@ -0,0 +1,25 @@
+import {} from "react";
+import "./Expertise_Card.scss";
+import Image from "next/image";
+interface ExpertiseData {
+  title: string;
+  content: string;
+  image: string;
+}
+function Expertise_Card({ data }: { data: ExpertiseData }) {
+  return (
+    <>
+      <div className={`card`}>
+        <Image src={data.image} alt={data.title} height={100} width={100} />
+        <div className="card__content backdrop-blur-sm p-5">
+          <h1 className="card__title font-bold">{data.title}</h1>
+          <p className="card__description sm:text-base text-sm">
+            {data.content}{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Expertise_Card;
