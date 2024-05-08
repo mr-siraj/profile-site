d6b0a1b69124400c42e9d31ba132764e073374a4
Zlaam <mrzalaam@gmail.com>
2024-05-08 20:56:49 +0500

    update page.tsx

--- src/app/(pages)/expertise/page.tsx
+++ src/app/(pages)/expertise/page.tsx
@@ -1,5 +1,22 @@
 export default function ExpertisePage() {
-  return <div className="relative">Expertise</div>;
+  return (
+    <section className="flex flex-col">
+      <div className="my-2 flex-1">
+        <h1 className="font-bold md:text-5xl text-3xl">My Expertise!</h1>
+        <h2 className="leading-[2rem] tracking-wider text-xl my-2">
+          I am proficient in languages such as JavaScript and Python, and I
+          specialize in creating dynamic and responsive web applications. My
+          goal is to continue pushing the boundaries of innovation in the
+          ever-evolving field of web development.
+        </h2>
+      </div>
+      <div className="my-2 flex-1 flex flex-col">
+        <h1 className="font-bold md:text-5xl text-3xl">
+          Technologies I use in Daily Life:-
+        </h1>
+      </div>
+    </section>
+  );
 }
 
 export const metadata = {
