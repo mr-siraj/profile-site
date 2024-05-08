2b2da114dca780530837c44e06f450c115a8da6e
Zlaam <mrzalaam@gmail.com>
2024-05-09 01:26:05 +0500

    build(deps): update package-lock.json, package.json and page.tsx

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,9 +1,45 @@
+import Link from "next/link";
 import {} from "react";
-
+import { BsMessenger } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa6";
+import { IoLogoGithub, IoLogoTwitter } from "react-icons/io5";
+import { LiaExternalLinkSquareAltSolid } from "react-icons/lia";
+import { MdEmail } from "react-icons/md";
 function ContactPage() {
+  const contactLinks = [
+    {
+      Icon: MdEmail,
+      IconName: "Email",
+      path: `mailto:mrzalaam@gmail.com`,
+    },
+    {
+      Icon: BsMessenger,
+      IconName: "Messenger",
+      path: `https://www.facebook.com/messages/t/100052341221564`,
+    },
+    {
+      Icon: FaLinkedin,
+      IconName: "LinkedIn",
+      path: `https://www.linkedin.com/in/mr-zlaam-29b944296/`,
+    },
+    {
+      Icon: IoLogoGithub,
+      IconName: "Github",
+      path: `https://github.com/mr-zlaam`,
+    },
+    {
+      Icon: IoLogoTwitter,
+      IconName: "Twitter",
+      path: `https://twitter.com/mr_zlaam`,
+    },
+  ];
   return (
     <>
-      <section>ContactPage</section>
+      <h1 className=" flex items-center justify-center h-10 text-center text-2xl">
+        <Link href={"#"} className="link_dark">
+          Read This Before Contact
+        </Link>
+      </h1>
     </>
   );
 }
