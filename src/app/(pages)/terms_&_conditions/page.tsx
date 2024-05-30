6351b336e532115cc04c18493bbdaa04d5b81f53
Zlaam <mrzalaam@gmail.com>
2024-05-30 09:22:37 +0500

    add page animation to terms&condition page

--- src/app/(pages)/terms_&_conditions/page.tsx
+++ src/app/(pages)/terms_&_conditions/page.tsx
@@ -4,7 +4,7 @@ import {} from "react";
 function Terms_And_Conditions() {
   return (
     <>
-      <section className=" h-[90dvh] flex justify-center items-center flex-col gap-10">
+      <section className=" h-[90dvh] animate-page flex justify-center items-center flex-col gap-10">
         <h1 className="md:text-5xl font-bold italic text-3xl text-center">
           We&apos;ll Update This Page Soon
         </h1>
