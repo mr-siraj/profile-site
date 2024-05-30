591b0bb9d6787a1ad10bc4ee96cda19b1b25aa95
Zlaam <mrzalaam@gmail.com>
2024-05-30 11:20:05 +0500

    ✨ feat(add pdf support and fixed broken links):

---
+++ src/app/(pages)/contact/components/ContactLink.ts
@@ -0,0 +1,31 @@
+import { BsMessenger, BsTwitterX } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
+export const contactLinks = [
+  {
+    Icon: MdEmail,
+    IconName: "Email",
+    path: `mailto:zlaam.dev@gmail.com`,
+  },
+  {
+    Icon: BsMessenger,
+    IconName: "Messenger",
+    path: `https://www.facebook.com/messages/t/100052341221564`,
+  },
+  {
+    Icon: FaLinkedin,
+    IconName: "LinkedIn",
+    path: `https://www.linkedin.com/in/mr-zlaam-29b944296/`,
+  },
+  {
+    Icon: IoLogoGithub,
+    IconName: "Github",
+    path: `https://github.com/mr-zlaam`,
+  },
+  {
+    Icon: BsTwitterX,
+    IconName: "-Twitter",
+    path: `https://x.com/mr_zlaam`,
+  },
+];
--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,37 +1,8 @@
 import Link from "next/link";
 import {} from "react";
-import { BsMessenger, BsTwitterX } from "react-icons/bs";
-import { FaLinkedin } from "react-icons/fa6";
-import { IoLogoGithub } from "react-icons/io5";
-import { MdEmail } from "react-icons/md";
+import { contactLinks } from "./components/ContactLink";
+
 function ContactPage() {
-  const contactLinks = [
-    {
-      Icon: MdEmail,
-      IconName: "Email",
-      path: `mailto:zlaam.dev@gmail.com`,
-    },
-    {
-      Icon: BsMessenger,
-      IconName: "Messenger",
-      path: `https://www.facebook.com/messages/t/100052341221564`,
-    },
-    {
-      Icon: FaLinkedin,
-      IconName: "LinkedIn",
-      path: `https://www.linkedin.com/in/mr-zlaam-29b944296/`,
-    },
-    {
-      Icon: IoLogoGithub,
-      IconName: "Github",
-      path: `https://github.com/mr-zlaam`,
-    },
-    {
-      Icon: BsTwitterX,
-      IconName: "Twitter",
-      path: `https://twitter.com/mr_zlaam`,
-    },
-  ];
   return (
     <section className="animate-page">
       <h1 className=" flex items-center justify-center h-10 text-center sm:text-2xl text-xl ">
