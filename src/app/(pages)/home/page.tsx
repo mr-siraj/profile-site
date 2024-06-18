e864f102409e6c069b8c1c91b2d49aedd200c0ab
Zlaam <mrzalaam@gmail.com>
2024-06-18 17:41:17 +0500

    "Moved metadata from layout.tsx to home/page.tsx and updated its content"

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -36,3 +36,10 @@ function HomePage() {
 }
 
 export default HomePage;
+export const metadata = {
+  title: "Zlaam | Home",
+  creator: "mr-zlaam",
+  publisher: "mr-zlaam",
+  description:
+    "I am Zlaam, a dedicated and skilled full-stack web app developer with several years of hands-on experience. My true passion lies in transforming concepts into tangible digital solutions, and I wholeheartedly embrace new challenges that come my way. I am proficient in languages such as JavaScript and Python, and I specialize in creating dynamic and responsive web applications. My goal is to continue pushing the boundaries of innovation in the ever-evolving field of web development. ",
+};
