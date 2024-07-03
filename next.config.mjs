dc9d4eed4ddb396eead3264456c0cc4e6eb3ffa7
Zlaam <mrzalaam@gmail.com>
2024-07-03 18:30:35 +0500

    "Remove custom webpack configuration and swcMinify option from next.config.mjs"

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [
       {
@@ -8,45 +8,6 @@ const nextConfig = {
         hostname: "**",
       },
     ],
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // Custom production optimizations
-      config.optimization = {
-        splitChunks: {
-          chunks: "all",
-          minSize: 20000,
-          maxSize: 70000,
-          minChunks: 1,
-          maxAsyncRequests: 30,
-          maxInitialRequests: 30,
-          automaticNameDelimiter: "~",
-          cacheGroups: {
-            vendors: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10,
-              reuseExistingChunk: true,
-            },
-            default: {
-              minChunks: 2,
-              priority: -20,
-              reuseExistingChunk: true,
-            },
-          },
-        },
-        runtimeChunk: {
-          name: "runtime",
-        },
-      };
-    }
-
-    // Additional custom Webpack configurations
-    config.module?.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
   },
 };
 
