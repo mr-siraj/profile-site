23ba73050878fb6d778f41e07ca2f5c712b48054
Zlaam <mrzalaam@gmail.com>
2024-07-03 18:34:46 +0500

    remove helloworld console from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 export default function middleware(request: NextRequest) {
-  console.log("helloworld");
   if (request.nextUrl.pathname === "/")
     return NextResponse.redirect(new URL("/home", request.url));
 }
